94fa8908bbb20cb5b44432ad903c43ea1e326e0d
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-29 11:53:50 +0530

    [Suraj]: Usecase 1 refactored.

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -1,3 +1,2 @@
 ﻿GenericProblems.Generic generic = new GenericProblems.Generic();
 Console.WriteLine("Maximum Integer is: "+ generic.findMaximumInteger(8, 9, 7));
-Console.WriteLine("Maximum Float is: "+ generic.findMaximumDouble(8.2, 7.4, 9.0));
